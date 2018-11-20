4a9892dd193c17fbf6225848b395cd33ffab0676
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-11-20 12:15:49 -0200

    Add pep8 style

--- setup.py
+++ setup.py
@@ -1,34 +1,37 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
-# NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+import setuptools
 
 
-version = '0.1'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(BASE_DIR, 'README.md')).read()
 
-install_requires = ["numpy"]
-
-setup(name='pdf-table-extract',
-      version=version,
-      description="Extract Tables from PDF files",
-      long_description=README + '\n\n',  # + NEWS,
-      classifiers=[
-          # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      ],
-      keywords='PDF, tables',
-      author='Ian McEwan',
-      author_email='ijm@ashimaresearch.com',
-      url='https://github.com/codeRuth/pdf-table-extract/',
-      download_url='https://github.com/codeRuth/pdf-table-extract/archive/0.1.tar.gz',
-      license='MIT-Expat',
-      packages=find_packages('src'),
-      package_dir={'': 'src'}, include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      entry_points={
-          'console_scripts':
-              ['pdf-table-extract=pdftableextract.scripts:main']
-      }
-      )
+setuptools.setup(
+    name='pdf-table-extract',
+    version='0.1.1',
+    description='Extract Tables from PDF files',
+    long_description=README,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+    keywords='PDF, tables',
+    author='Ian McEwan',
+    author_email='ijm@ashimaresearch.com',
+    url='https://github.com/codeRuth/pdf-table-extract/',
+    download_url=(
+        'https://github.com/codeRuth/pdf-table-extract/archive/0.1.tar.gz'
+    ),
+    license='MIT-Expat',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['numpy'],
+    entry_points={
+        'console_scripts': ['pdf-table-extract=pdftableextract.scripts:main']
+    }
+)
--- src/pdftableextract/__init__.py
+++ src/pdftableextract/__init__.py
@@ -1,2 +1,21 @@
-# Example package with a console entry point
-from core import process_page, output, table_to_list
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""PDF Table Extraction Utility"""
+
+from .core import process_page, output, table_to_list
+
+
+__author__ = 'Ian McEwan, Ashima Research'
+__copyright__ = (
+    'Copyright (C) 2011 Ashima Research. All rights reserved. '
+    'Distributed under the MIT Expat License. '
+    'See LICENSE file. https://github.com/ashima/pdf-table-extract'
+)
+__credits__ = ['Ian McEwan', 'Ashima Research', 'Ronald Theodoro']
+__license__ = 'MIT Expat'
+__version__ = '0.1.1'
+__maintainer__ = 'Ronald Theodoro'
+__status__ = 'Development'
+
+__all__ = ['process_page', 'output', 'table_to_list']
